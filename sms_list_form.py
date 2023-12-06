d5a8376eff3a24e7b6697a735cf951851e18311f
John Gordon <jcg4sg@virginia.edu>
2023-12-06 17:50:28 -0500

    Update sms_list_form.py

--- sms_list_form.py
+++ sms_list_form.py
@@ -28,7 +28,7 @@ def sidebar_bg(side_bg):
       )
 side_bg = 'image.jpg'
 sidebar_bg(side_bg)
-# st.image('image.jpg')
+st.image('image.jpg')
 
 with col1:
     first = st.text_input('First Name:')
@@ -40,7 +40,6 @@ num = st.text_input('Phone Number:')
 
 send = st.button('Add Yourself to Our List!')
 
-st.image('image.jpg')
 
 if send and first and last and num:
     message = client.messages \
