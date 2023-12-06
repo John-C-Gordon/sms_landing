ce9b474b434dad68afde7e632f818c2524ede7c6
John Gordon <jcg4sg@virginia.edu>
2023-12-05 19:44:24 -0500

    Add files via upload
    First commit

---
+++ sms_list_form.py
@@ -0,0 +1,38 @@
+# Download the helper library from https://www.twilio.com/docs/python/install
+import os
+from twilio.rest import Client
+import streamlit as st
+
+# Find your Account SID and Auth Token at twilio.com/console
+# and set the environment variables. See http://twil.io/secure
+account_sid = 'AC9013123df2b0b98901639b236ce3a1f4'
+auth_token = '90e158ced56010db6ab13e56a9438fea'
+client = Client(account_sid, auth_token)
+st.title("You're Almost There!")
+col1, col2 = st.columns(2)
+
+
+
+with col1:
+    first = st.text_input('First Name:')
+
+with col2:
+    last = st.text_input('Last Name:')
+
+num = st.text_input('Phone Number:')
+
+send = st.button('Add Yourself to Our List!')
+
+if send and first and last and num:
+    message = client.messages \
+                    .create(
+                        body='NEW SUBSCRIBER:\n' + '{}'.format(first) + ' ' + '{}'.format(last) + ': {}'.format(num),
+                        from_='+18558677021',
+                        to='+17577105116'
+                    )
+
+    st.write('*Got it! You should receive a confirmation from in a few moments. If you do not hear from us within 24 hours, please reach out to cougarbeatrice@gmail.com.*')
+
+if send and (not first or not last or not num):
+    st.write('*Please fill in all fields.*')
+
